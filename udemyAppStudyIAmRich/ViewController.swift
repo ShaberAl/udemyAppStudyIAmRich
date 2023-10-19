27d7f2169acdc2268615e0cab2e113ee6b28300a
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-10-19 20:52:25 +0500

    Initial Commit

---
+++ udemyAppStudyIAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  udemyAppStudyIAmRich
+//
+//  Created by MacOS on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
